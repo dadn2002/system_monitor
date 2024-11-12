7cc20aa8e7b5502b3e77e4cbe359202b3ffd8edb
Davi Nascimento <83386563+dadn2002@users.noreply.github.com>
2024-11-11 22:43:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 
+import os
+from header import *
+from generate_graphs import generate_graphs_main
 
 if __name__ == "__main__":
+    os.system("cls")
+    info(f"Running code in terminal")
+    generate_graphs_main()
     pass    
